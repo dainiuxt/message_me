4b1f2b6f991517e36cbdcfd1fc669eaaf075ed7d
dainiuxt <dainiuxt@gmail.com>
2020-08-04 21:15:23 +0300

    added basic authentication functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,4 +3,22 @@ class SessionsController < ApplicationController
 	def new
 	end
 
+	def create
+		user = User.find_by(username: params[:session][:username])
+		if user && user.authenticate(params[:session][:password])
+			session[:user_id] = user.id
+			flash[:success] = "Welcome to MessageMe!"
+			redirect_to root_path
+		else
+			flash.now[:error] = "Incorrect username or password"
+			render 'new'
+		end
+	end
+
+	def destroy
+		session[:user_id] = nil
+		flash[:success] = "You have loged out. Bye."
+		redirect_to login_path
+	end
+
 end
